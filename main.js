403b3ebb02df2ee817c030fa0b5154c3f8ffa741
corradocoppo <corradocoppolino@gmail.com>
2021-04-19 15:59:34 +0200

    boh

--- main.js
+++ main.js
@@ -1,25 +1,19 @@
 $(document).ready(function(){
 
-    
-
     $(".header-right").click(function(){
 
         var x = $(".hamburger-menu");
         
         if (x.style.display === "none") {
+
             x.style.display = "block";
-          } else {
+
+        } else {
+
             x.style.display = "none";
-          }
+
+        }
         
-    
     })
-
-    
-
-
-
-
-
 
 })
